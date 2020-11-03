bdae18c493d54dba7ce67b8e17fbaa4fd0b2184d
Shawn Becker <shawn@yptokey.com>
2020-11-03 13:59:12 +0100

    moved functions to seperate package

--- main.go
+++ main.go
@@ -1,55 +1,20 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
+	"u_admin/backend"
+
+	_ "github.com/go-sql-driver/mysql"
 )
-
-type User struct {
-	ID           int    "json:_id"
-	FirstName    string "json:firstName"
-	LastName     string "json:lastName"
-	Instructor   bool   "json:instructor"
-	Participated int    "json:participatedIn"
-	Cunducted    int    "json:conducted"
-}
-
-var Users []User
-
-func getAllUsers(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Get All Users Endpoint Hit")
-	json.NewEncoder(w).Encode(Users)
-}
 
 func main() {
 
-	Users = []User{
-		User{
-			ID:           1,
-			FirstName:    "Shawn",
-			LastName:     "Becker",
-			Instructor:   true,
-			Participated: 2,
-			Cunducted:    0,
-		}, User{
-			ID:           2,
-			FirstName:    "Gordon",
-			LastName:     "Becker",
-			Instructor:   false,
-			Participated: 0,
-			Cunducted:    2,
-		},
-	}
+	backend.InitDb()
 
-	fmt.Println("Starting server on Port 8000")
+	router := backend.NewRouter()
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// do something
-	})
-
-	http.HandleFunc("/users", getAllUsers)
-
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	log.Fatal(http.ListenAndServe(":8000", router))
 }
+
+// db functions
