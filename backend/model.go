71a82ac0569ef51891d017de0ec6e2195176cd3e
Shawn Becker <shawn@yptokey.com>
2020-11-11 14:54:47 +0100

    Edit  naming scheme

--- backend/model.go
+++ backend/model.go
@@ -2,10 +2,10 @@ package backend
 
 // modal for user
 type User struct {
-	ID             string `json:"_id`
-	Firstname      string `json:"firstName"`
-	Lastname       string `json:"lastName"`
-	Instructor     bool   `json:"instructor"`
+	ID             string `json:"UserId`
+	Firstname      string `json:"FirstName"`
+	Lastname       string `json:"LastName"`
+	Instructor     bool   `json:"Instructor"`
 	Workshopsgiven int    `json:"workshopsGiven"`
 }
 
