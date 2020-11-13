15e9186747c76c1ea29660d1beabecf50c38c3c9
Shawn Becker <shawn@yptokey.com>
2020-11-13 13:17:21 +0100

    CORS handlers for development

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"u_admin/backend"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/handlers"
 )
 
 func main() {
@@ -14,7 +15,12 @@ func main() {
 
 	router := backend.NewRouter()
 
-	log.Fatal(http.ListenAndServe("localhost:8000", router))
+	allowedOrigins := handlers.AllowedOrigins([]string{"*"})
+	allowedMethods := handlers.AllowedMethods([]string{"GET", "POST", "DELETE", "PUT"})
+	allowedHeaders := handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"})
+
+	log.Fatal(http.ListenAndServe("localhost:8000",
+		handlers.CORS(allowedOrigins, allowedMethods, allowedHeaders)(router)))
 }
 
 // db functions
